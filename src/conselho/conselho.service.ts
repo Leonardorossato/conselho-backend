f185f473f57809cad4df78aaf890b214aa43ca04
Leonardorossato <leonardorossato21@gmail.com>
2022-12-06 13:50:18 -0300

    dto and services

--- src/conselho/conselho.service.ts
+++ src/conselho/conselho.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateConselhoDto } from './dto/create.conselho.dto';
 import { Conselho } from './entities/conselho.entity';
 
 @Injectable()
@@ -13,6 +14,15 @@ export class ConselhoService {
             return conselho
         } catch (error) {
             throw new HttpException('Conselho not found', HttpStatus.NOT_FOUND)
+        }
+    }
+
+    async create(dto: CreateConselhoDto){
+        try {
+            const conselho = await this.conselhoRepository.save(dto)
+            return conselho
+        } catch (error) {
+            throw new HttpException('Erro to create conselho', HttpStatus.BAD_REQUEST)
         }
     }
 }
