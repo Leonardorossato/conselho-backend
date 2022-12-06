cbac88e0e2755ef521af48710032246a199fc324
Leonardorossato <leonardorossato21@gmail.com>
2022-12-06 10:22:11 -0300

    email entity

---
+++ src/email/entities/email.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Email{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({nullable: false, unique: true})
+    email: string
+
+    @Column()
+    nome: string
+}
