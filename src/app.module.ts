30f082fd8131eba43dc801dab33bb9ed1e7b2de5
Leonardorossato <leonardorossato21@gmail.com>
2022-12-06 11:06:01 -0300

    imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './config/ormconfig';
 import { ConselhoModule } from './conselho/conselho.module';
 import { EmailModule } from './email/email.module';
+import { EmailEnviadoModule } from './email-enviado/email-enviado.module';
 
 @Module({
   imports: [ TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
-    ConfigModule.forRoot({ isGlobal: true }),ConselhoModule, EmailModule]
+    ConfigModule.forRoot({ isGlobal: true }),ConselhoModule, EmailModule, EmailEnviadoModule]
 })
 export class AppModule {}
