7e2535f59f67c5a347bda88f412553974780fde1
Leonardorossato <leonardorossato21@gmail.com>
2022-12-06 10:36:42 -0300

    typeorm module and config orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './config/ormconfig';
 import { ConselhoModule } from './conselho/conselho.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [ConselhoModule, EmailModule]
+  imports: [ TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    ConfigModule.forRoot({ isGlobal: true }),ConselhoModule, EmailModule]
 })
 export class AppModule {}
