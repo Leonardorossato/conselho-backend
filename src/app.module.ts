6a26b3fa1253b472f1d5db31a1157e5240ab37ae
Leonardorossato <leonardorossato21@gmail.com>
2022-12-06 10:13:50 -0300

    import conselho

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConselhoModule } from './conselho/conselho.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConselhoModule]
 })
 export class AppModule {}
