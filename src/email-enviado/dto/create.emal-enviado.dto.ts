eb158a0b78c59f7eaf7752dce68e3926626b95a4
Matheusesp1 <matheusespindola120@gmail.com>
2022-12-06 14:59:53 -0300

    service email done

--- src/email-enviado/dto/create.emal-enviado.dto.ts
+++ src/email-enviado/dto/create.emal-enviado.dto.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
-export class CreateEmailEnviadoDto{
+export class CreateEmailEnviadoDto{ 
 
     @ApiProperty({example: '12/10/1999'})
     data: Date
