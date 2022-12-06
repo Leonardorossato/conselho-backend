aaaa764d0f4dc8e078a96c9faf99e5ef223bcb77
Leonardorossato <leonardorossato21@gmail.com>
2022-12-06 10:24:02 -0300

    email files

---
+++ src/email/email.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { EmailController } from './email.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Email } from './entities/email.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Email])],
+  controllers: [EmailController],
+  providers: [EmailService]
+})
+export class EmailModule {}
