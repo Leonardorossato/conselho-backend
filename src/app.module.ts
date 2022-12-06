9b5da202c3c14b70451c32fd654ae561b1153883
Leonardorossato <leonardorossato21@gmail.com>
2022-12-06 10:23:54 -0300

    imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConselhoModule } from './conselho/conselho.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [ConselhoModule]
+  imports: [ConselhoModule, EmailModule]
 })
 export class AppModule {}
