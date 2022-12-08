8416fa8d4cf4f12fec4c6bde9662a95e84c3e48b
Leonardorossato <leonardorossato21@gmail.com>
2022-12-08 13:29:26 -0300

    clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,6 @@ import { typeOrmAsyncConfig } from './config/ormconfig';
 import { ConselhoModule } from './conselho/conselho.module';
 import { EmailModule } from './email/email.module';
 import { EmailEnviadoModule } from './email-enviado/email-enviado.module';
-import { join } from 'path';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { MailerModule } from '@nestjs-modules/mailer';
 @Global()
 @Module({
   imports: [TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
@@ -15,30 +12,6 @@ import { MailerModule } from '@nestjs-modules/mailer';
     ConselhoModule, 
     EmailModule, 
     EmailEnviadoModule,
-    MailerModule.forRootAsync({
-    useFactory: async (config: ConfigService): Promise<any> => ({
-      transport: {
-        host: config.get('MAIL_HOST'),
-        secure: false,
-        port: config.get('MAIL_PORT'),
-        auth: {
-          user: config.get('MAIL_USER'),
-          pass: config.get('MAIL_PASS'),
-        },
-      },
-      defaults: {
-        from: `"No Reply" <${config.get('MAIL_FROM')}>`,
-      },
-      template: {
-        dir: join(__dirname + '/templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
-    inject: [ConfigService],
-    })
   ]
 })
 export class AppModule {}
