11ade60d9cd26aaeebd421b4d99f21e0a216b0b0
Leonardorossato <leonardorossato21@gmail.com>
2022-12-10 09:41:27 -0300

    mudança na migration estava dando erro

--- src/migrations/1670335500792-emailEnviado.ts
+++ src/migrations/1670335500792-emailEnviado.ts
@@ -5,13 +5,9 @@ export class emailEnviado1670335500792 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "email_enviado" ("id" SERIAL NOT NULL, "data" character varying NOT NULL, "emailId" integer, "conselhoId" integer, CONSTRAINT "PK_9c979eb4600cc424c635221f60d" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "email_enviado" ADD CONSTRAINT "FK_db258dad5caedb1c291f631ee16" FOREIGN KEY ("emailId") REFERENCES "email"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "email_enviado" ADD CONSTRAINT "FK_a65d250f7d1c72a348f2be5d152" FOREIGN KEY ("conselhoId") REFERENCES "conselho"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "email_enviado" DROP CONSTRAINT "FK_a65d250f7d1c72a348f2be5d152"`);
-        await queryRunner.query(`ALTER TABLE "email_enviado" DROP CONSTRAINT "FK_db258dad5caedb1c291f631ee16"`);
         await queryRunner.query(`DROP TABLE "email_enviado"`);
     }
 
