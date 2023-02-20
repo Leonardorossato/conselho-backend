a5a79d3034039e51dbbf3ba49733b8f888f4c0da
Leonardorossato <leonardorossato21@gmail.com>
2023-02-20 13:47:57 -0300

    update email.service.ts ready to use

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,38 +1,44 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { CACHE_MANAGER, HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { randomBytes } from 'crypto';
 import { Repository } from 'typeorm';
-import { CreateEmailDto } from './dto/create.email.dto';
+import { CreateEmailInput } from './dto/create-email.input';
 import { Email } from './entities/email.entity';
+import { EmailHelper } from './helpers/email.helper';
 
 @Injectable()
 export class EmailService {
-    constructor(@InjectRepository(Email) private emailRepository: Repository<Email>,
-    ) { }
+  constructor(
+    @InjectRepository(Email) private emailRepository: Repository<Email>,
+    private readonly emailHelper: EmailHelper,
+  ) {}
 
-    async all(): Promise<Email[]> {
-        try {
-            const email = await this.emailRepository.find()
-            return email
-        } catch (error) {
-            throw new HttpException('Email not found', HttpStatus.NOT_FOUND)
-        }
+  async findAll(): Promise<Email[]> {
+    try {
+      const email = await this.emailRepository.find();
+      return email;
+    } catch (error) {
+      throw new HttpException(
+        'Error to find all emails',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    
-    async create(dto: CreateEmailDto) {
-        try {
-            const user = await this.emailRepository.findOneBy({ email: dto.email })
-            if (user) {
-                throw new HttpException('Email already existis', HttpStatus.BAD_REQUEST)
-            }
-            const email = await this.emailRepository.create(dto)
-            await this.emailRepository.save(email)
-            return email
-        } catch (error) {
-            throw new HttpException('Erro to create email', HttpStatus.BAD_REQUEST)
-        }
+  }
+
+  async create(dto: CreateEmailInput) {
+    try {
+      const user = await this.emailRepository.findOneBy({ email: dto.email });
+      if (user) {
+        throw new HttpException(
+          'Email already existis',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      const email = await this.emailRepository.create(dto);
+      await this.emailHelper.formatEmailResponse([email]);
+      await this.emailRepository.save(email);
+      return email;
+    } catch (error) {
+      throw new HttpException('Erro to create a email', HttpStatus.BAD_REQUEST);
     }
+  }
 }
-
-
