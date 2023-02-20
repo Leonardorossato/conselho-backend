89a58a3a7bfbced9da8abe3b795e0f55b2f38afa
Leonardorossato <leonardorossato21@gmail.com>
2023-02-20 13:28:51 -0300

    feat: create conselho.resolver.ts reolver to use with graphQL

---
+++ src/conselho/conselho.resolver.ts
@@ -0,0 +1,29 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ConselhoService } from './conselho.service';
+import { CreateConselhoInput } from './dto/create-conselho.input';
+import { ConselhoSchema } from './schema/conselho.schema';
+
+@Resolver(() => ConselhoSchema)
+export class ConselhoResolver {
+  constructor(private readonly conselhoService: ConselhoService) {}
+
+  @Query(() => [ConselhoSchema], { name: 'conselho' })
+  async findAll() {
+    return await this.conselhoService.findAll();
+  }
+
+  @Query(() => [ConselhoSchema], { name: 'getconselhoApi' })
+  async getConselhoApi() {
+    return await this.conselhoService.getConselhoApi();
+  }
+
+  @Query(() => [ConselhoSchema], { name: 'conselhoAleatorio' })
+  async conselhoAleatorio() {
+    return await this.conselhoService.getConselhoAleatorio();
+  }
+
+  @Query(() => [ConselhoSchema], { name: 'translate' })
+  async translate() {
+    return await this.conselhoService.translateConselho();
+  }
+}
