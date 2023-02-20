f5caf6a5060c0e8713a0643c7de949ca87ba8fb0
Leonardorossato <leonardorossato21@gmail.com>
2023-02-20 13:28:30 -0300

    update conselho.module.ts updat imports

--- src/conselho/conselho.module.ts
+++ src/conselho/conselho.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConselhoService } from './conselho.service';
-import { ConselhoController } from './conselho.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Conselho } from './entities/conselho.entity';
 import { ConfigModule } from '@nestjs/config';
+import { ConselhoHelper } from './helpers/conselho.helper';
+import { ConselhoResolver } from './conselho.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Conselho]), ConfigModule],
-  controllers: [ConselhoController],
-  providers: [ConselhoService],
-  exports: [ConselhoService]
+  providers: [ConselhoService, ConselhoHelper, ConselhoResolver],
 })
 export class ConselhoModule {}
